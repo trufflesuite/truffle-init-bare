ced9297ac2512dffd81904065cc10202931e13cd
g. nicholas d'andrea <gnidan@users.noreply.github.com>
2018-11-16 19:42:42 -0500

    Specify gas and turn on optimizer

--- truffle.js
+++ truffle.js
@@ -3,7 +3,18 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+      gas: 6721975
+    }
+  },
+  compilers: {
+    solc: {
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
     }
   }
 };
